8a90ae5272736daea8f4a695b3f29ef8cebf7968
Petar Sekulić <petar77@live.com>
2023-02-25 23:28:03 +0100

    Fix evaluate method

--- rsna_engine.py
+++ rsna_engine.py
@@ -2,7 +2,7 @@ import sys
 import math
 import numpy as np
 import torch
-from torch.autograd import Variable
+from random import shuffle
 from timm.utils import accuracy
 from rsna_utils import (
     load_image,
@@ -60,15 +60,17 @@ def run_iteration(
                 for patch in patch_generator(img, patch_size):
                     patches.append(patch)
 
-            patches = patches[:max_num_patches]
+            patches = np.array(patches[:max_num_patches])
+            patch_is = np.array([i for i in range(len(patches))])
+            shuffle(patch_is)
             for p in range(0, len(patches), patches_per_in_iter):
-                b_patches = patches[p:p+patches_per_in_iter]
+                z_matrix = z_matrix.detach()
+                b_patches = patches[patch_is[p:p+patches_per_in_iter]]
                 torch_image = torch.from_numpy(np.stack(b_patches, axis=0)).float().to(device)
                 z = q1_model(torch_image)
-                z_matrix_variable = Variable(z_matrix)
-                z_matrix_variable[patient_ind, p:p+len(b_patches)] = z
+                z_matrix[patient_ind, patch_is[p:p+len(b_patches)]] = z
 
-                y_pred = q2_model(z_matrix_variable, key_padding_mask)
+                y_pred = q2_model(z_matrix, key_padding_mask)
                 loss = criterion(y_pred, labels) / grad_acc_steps
                 loss.backward(retain_graph=False)
             patches = []
@@ -132,7 +134,20 @@ def train_one_epoch(
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
 
 @torch.no_grad()
-def evaluate(df, img_path, data_loader, q1_model, q2_model, patch_size, device):
+def evaluate(
+    df,
+    img_path,
+    data_loader,
+    q1_model,
+    q2_model,
+    patch_size,
+    device,
+    max_num_patches=32,
+    img_mean=0,
+    img_std=1
+):
+    q1_model.eval()
+    q2_model.eval()
     criterion = torch.nn.CrossEntropyLoss()
 
     metric_logger = MetricLogger(delimiter="  ")
@@ -141,7 +156,17 @@ def evaluate(df, img_path, data_loader, q1_model, q2_model, patch_size, device):
     for p_ids, target in metric_logger.log_every(data_loader, 10, header):
         target = target.to(device, non_blocking=True)
 
-        z_matrix, key_padding_mask = z_filling(df, img_path, p_ids, q1_model, patch_size, device)
+        z_matrix, key_padding_mask = z_filling(
+            df,
+            img_path,
+            p_ids,
+            q1_model,
+            patch_size,
+            device,
+            max_num_patches=max_num_patches,
+            img_mean=img_mean,
+            img_std=img_std
+        )
         output = q2_model(z_matrix, key_padding_mask)
         loss = criterion(output, target)
 
